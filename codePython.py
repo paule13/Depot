7f319b1ee1965757973b83bf950b92e88f1c8567
EtienneBost <55285250+EtienneBost@users.noreply.github.com>
2021-04-28 21:21:18 +0200

    Update codePython.py

--- codePython.py
+++ codePython.py
@@ -454,7 +454,60 @@ def inter_to_tc_2(inter, tc1,tc2):
     return newtab
         
 
+def matrice_tkinder (matrice):
+    root = tk.Tk()
+    root.grid_rowconfigure(0, weight=1)
+    root.columnconfigure(0, weight=1)
 
+    frame_main = tk.Frame(root, bg="gray")
+    frame_main.grid(sticky='news')
+
+
+    # Create a frame for the canvas with non-zero row&column weights
+    frame_canvas = tk.Frame(frame_main)
+    frame_canvas.grid(row=2, column=0, pady=(5, 0), sticky='nw')
+    frame_canvas.grid_rowconfigure(0, weight=1)
+    frame_canvas.grid_columnconfigure(0, weight=1)
+    # Set grid_propagate to False to allow 5-by-5 buttons resizing later
+    frame_canvas.grid_propagate(False)
+
+    # Add a canvas in that frame
+    canvas = tk.Canvas(frame_canvas, bg="yellow")
+    canvas.grid(row=0, column=0, sticky="news")
+
+    # Link a scrollbar to the canvas
+    vsb = tk.Scrollbar(frame_canvas, orient="vertical", command=canvas.yview)
+    hsb = tk.Scrollbar(frame_canvas, orient="horizontal", command=canvas.yview)
+    vsb.grid(row=0, column=1, sticky='ns')
+    hsb.grid(row=1, column=0, sticky='ew')
+    canvas.configure(yscrollcommand=vsb.set,xscrollcommand=hsb.set)
+    # Create a frame to contain the buttons
+    frame_buttons = tk.Frame(canvas, bg="blue")
+    canvas.create_window((0, 0), window=frame_buttons, anchor='nw')
+
+    # Add 9-by-5 buttons to the frame
+    rows = 20#len(matrice)
+    columns = 20#len(matrice[0])
+    buttons = [[tk.Button() for j in range(columns)] for i in range(rows)]
+    for i in range(0, rows):
+        for j in range(0, columns):
+            buttons[i][j] = tk.Button(frame_buttons, text=(matrice[i][j]))
+            buttons[i][j].grid(row=i, column=j, sticky='news')
+
+    # Update buttons frames idle tasks to let tkinter calculate buttons sizes
+    frame_buttons.update_idletasks()
+
+    # Resize the canvas frame to show exactly 5-by-5 buttons and the scrollbar
+    first5columns_width = sum([buttons[0][j].winfo_width() for j in range(0, int(columns/2) )])
+    first5rows_height = sum([buttons[i][0].winfo_height() for i in range(0, int(rows/2) )])
+    frame_canvas.config(width=first5columns_width + vsb.winfo_width(),
+    height=first5rows_height)
+
+    # Set the canvas scrolling region
+    canvas.config(scrollregion=canvas.bbox("all"))
+
+    # Launch the GUI
+    root.mainloop()
 
 file_path="./music1.wav"
 
